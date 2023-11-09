696f66222e033c2da6bc45d6ef293dd328bcfa88
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2023-11-09 08:57:21 -0300

    commit

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,10 +1,28 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-projetos',
   templateUrl: './projetos.component.html',
   styleUrls: ['./projetos.component.css']
 })
-export class ProjetosComponent{
+export class ProjetosComponent implements OnInit{
+  currentIndex = 0;
+  images: string[] = [
+    'assets/img/java.png',
+    'assets/img/css.png',
+    'assets/img/html.png'
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  prevImage(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
